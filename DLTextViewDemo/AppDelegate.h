b292300a7596bd85520bca90252a222b40f533b2
邹道利 <zoudaoli@163.com>
2016-06-21 13:43:20 +0800

    Initial Commit

---
+++ DLTextViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DLTextViewDemo
+//
+//  Created by 邹道利 on 21/6/16.
+//  Copyright © 2016年 www.visualogies.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DLTextViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DLTextViewDemo
+//
+//  Created by 邹道利 on 21/6/16.
+//  Copyright © 2016年 www.visualogies.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
